34d4af2c125271e4f840fac836056180af617b96
Hayden James Cooper <haydencpr95@gmail.com>
2023-05-30 20:13:03 -0400

    Console.log Updaet, still bug with order and first running module

--- vendor/index.js
+++ vendor/index.js
@@ -3,7 +3,6 @@
 const eventPool = require('../eventPool');
 const Chance = require('chance');
 
-// Simulate a pickup event for the given store name
 function simulatePickup(storeName) {
   const payload = generateOrderPayload(storeName);
   eventPool.emit('pickup', payload);
@@ -14,7 +13,6 @@ function simulatePickup(storeName) {
   });
 }
 
-// Generate a random order payload using chance library
 function generateOrderPayload(storeName) {
   const chance = new Chance();
   const orderId = chance.guid();
@@ -31,12 +29,9 @@ function generateOrderPayload(storeName) {
   };
 }
 
-// Function to process the event data and retrieve the desired information
 function processEventData(event) {
   if (event.event === 'pickup') {
-    // Process the pickup event
     const { orderId, customer, address } = event.payload;
-    // Return the desired data
     return {
       orderId,
       customer,
@@ -44,17 +39,13 @@ function processEventData(event) {
       status: 'picked up',
     };
   } else if (event.event === 'in-transit') {
-    // Process the in-transit event
     const { orderId } = event.payload;
-    // Return the desired data
     return {
       orderId,
       status: 'in transit',
     };
   } else if (event.event === 'delivered') {
-    // Process the delivered event
     const { orderId } = event.payload;
-    // Return the desired data
     return {
       orderId,
       status: 'delivered',
@@ -64,11 +55,10 @@ function processEventData(event) {
 
 module.exports = {
   start: function () {
-    // Start the vendor client application
     const storeName = '1-206-flowers';
     setInterval(() => {
       simulatePickup(storeName);
     }, 5000);
   },
-  processEventData: processEventData, // Export the processEventData function
+  processEventData: processEventData, 
 };
